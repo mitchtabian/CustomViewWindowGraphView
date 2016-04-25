bbf8e67d599ef647a7c54193b2a9551ac0ae1a09
mitchtabian <mitch@tabian.ca>
2016-04-24 22:00:14 -0700

    Source Code

---
+++ app/src/main/java/practice/application/graphviewtutorial2/MainActivity.java
@@ -0,0 +1,44 @@
+package practice.application.graphviewtutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class MainActivity extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        double y,x;
+        x = -5.0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i = 0; i<500; i++){
+            x = x + 0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+        graph.addSeries(series);
+
+        // set manual X bounds
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(-5.0);
+        graph.getViewport().setMaxX(5.0);
+
+        // set manual Y bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-2.0);
+        graph.getViewport().setMaxY(2.0);
+
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setScalable(true);
+    }
+}
+
